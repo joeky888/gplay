67d918563c81659a7fa3ba5dcf2a9640b16d428b
Joeky Zhan <jj16180339887@gmail.com>
2018-09-23 00:47:47 +0800

    Debug gst
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

--- gst/gst.go
+++ gst/gst.go
@@ -8,14 +8,37 @@ package gst
 */
 import "C"
 import (
+	"os"
+	"syscall"
+	"os/signal"
 	"fmt"
 	"sync"
 	"unsafe"
 
+	"github.com/joeky888/gplay/opusdec"
+	"github.com/joeky888/gplay/alsa"
 	"github.com/pions/webrtc"
 )
 
+const (
+	videoClockRate = 90000
+	audioClockRate = 48000
+	channels = 1
+	format = alsa.FormatS16LE
+)
+
+var player *alsa.PlaybackDevice
+var ctrlc = make(chan os.Signal)
+
 func init() {
+	var err error
+	player, err = alsa.NewPlaybackDevice("default", channels, format, audioClockRate,
+		alsa.BufferParams{BufferFrames: 0, PeriodFrames: 960, Periods: 960})
+	if err != nil {
+		panic(err)
+	}
+	signal.Notify(ctrlc, os.Interrupt, syscall.SIGTERM)
+	go cleanup(player)
 	go C.gstreamer_send_start_mainloop()
 }
 
@@ -73,28 +96,43 @@ func (p *Pipeline) Stop() {
 	C.gstreamer_send_stop_pipeline(p.Pipeline)
 }
 
-const (
-	videoClockRate = 90000
-	audioClockRate = 48000
-)
-
 //export goHandlePipelineBuffer
 func goHandlePipelineBuffer(buffer unsafe.Pointer, bufferLen C.int, duration C.int, pipelineID C.int) {
 	pipelinesLock.Lock()
 	defer pipelinesLock.Unlock()
 
+	pcm := make([]int16, 960)
+	dec, err := opusdec.NewDecoder(audioClockRate, 2)
+	if err != nil {
+		panic(err)
+	}
+
 	if pipeline, ok := pipelines[int(pipelineID)]; ok {
 		var samples uint32
 		if pipeline.codecName == webrtc.Opus {
 			samples = uint32(audioClockRate * (float32(duration) / 1000000000))
+			// fmt.Println(C.GoBytes(buffer, bufferLen))
+			// fmt.Println(samples)
+			_, err := dec.Decode(C.GoBytes(buffer, bufferLen), pcm)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(pcm)
+			player.Write(pcm)
 		} else {
 			samples = uint32(videoClockRate * (float32(duration) / 1000000000))
 		}
 		pipeline.in <- webrtc.RTCSample{Data: C.GoBytes(buffer, bufferLen), Samples: samples}
-		fmt.Println(C.GoBytes(buffer, bufferLen))
-		fmt.Println(samples)
 	} else {
 		fmt.Printf("discarding buffer, no pipeline with id %d", int(pipelineID))
 	}
 	C.free(buffer)
+}
+
+func cleanup(player *alsa.PlaybackDevice) {
+	// User hit Ctrl-C, clean up
+	<-ctrlc
+	fmt.Println("Close devices")
+	player.Close()
+	os.Exit(1)
 }
